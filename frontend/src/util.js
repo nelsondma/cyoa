e7c4a20c87d6213a563d95a7720b3926d1f5c7ec
Nelson Munoz <nelsondma@gmail.com>
2025-10-30 19:59:01 -0400

    Updating URL to longer one. I have no idea why Choreo changed it.

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "/choreo-apis/nm-cyoa/backend/v1/api";
+export const API_BASE_URL = "https://f6fad24c-8d70-41f5-9055-ed80d1def1a3-dev.e1-us-east-azure.choreoapis.dev/nm-cyoa/backend/v1/api";
 
